9c4f7f049c05d05e81e8e85ea16f4ca6f91bf885
Anton K. <antonkarlovskiy@outlook.com>
2024-03-12 20:10:38 +0100

    refactor: organize the code

---
+++ src/config/keys.ts
@@ -0,0 +1,5 @@
+const INFURA_API_KEY = '529882cf33164ad9b6d20b276b8f9a71'; // TODO: process.env.INFURA_API_KEY
+
+export {
+  INFURA_API_KEY
+};
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -2,25 +2,21 @@ import {
   JsonRpcProvider,
   Network
 } from 'ethers';
+import { ChainId } from '@uniswap/sdk-core';
 
-const getProvider = (): JsonRpcProvider => {
-  const INFURA_API_KEY = '529882cf33164ad9b6d20b276b8f9a71'; // TODO: process.env.INFURA_API_KEY
-  const network = 1; // TODO: hardcoded
-  let provider: JsonRpcProvider;
+import { INFURA_API_KEY } from '../config/keys';
 
-  if (INFURA_API_KEY) {
-    provider =
-      new JsonRpcProvider(
-        `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-        // RE: https://github.com/ethers-io/ethers.js/issues/4377#issuecomment-1837559329
-        Network.from(network),
-        { staticNetwork: true }
-      );
-  } else {
-    throw new Error('Something went wrong!');
+const getProvider = () => {
+  if (!INFURA_API_KEY) {
+    throw new Error(`Infura API key is undefined: ${INFURA_API_KEY}`);
   }
 
-  return provider;
+  return  new JsonRpcProvider(
+      `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
+      // RE: https://github.com/ethers-io/ethers.js/issues/4377#issuecomment-1837559329
+      Network.from(ChainId.MAINNET),
+      { staticNetwork: true }
+    );
 }
 
 export {
