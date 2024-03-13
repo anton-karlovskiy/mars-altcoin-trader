3f3244861043b6006aa1fe67cf39cff3e8fb09f9
Anton K. <antonkarlovskiy@outlook.com>
2024-03-13 14:46:06 +0100

    feat: implement createPair function

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,27 @@
 import {
   ChainId,
-  Token
+  Token,
+  WETH9
 } from '@uniswap/sdk-core';
 
 import {
   getProvider,
-  getDecimals
+  getDecimals,
+  createPair
 } from '@/utils/helpers';
 
 const main = async () => {
-  const mainnetId = ChainId.MAINNET;
-  const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
-  const daiDecimals = 18;
-  const DAI = new Token(mainnetId, daiAddress, daiDecimals);
-  console.log(DAI);
-  
-  const provider = getProvider();
+  const targetChainId = ChainId.MAINNET;
+  const provider = getProvider(targetChainId);
 
-  const decimals = await getDecimals(daiAddress, provider);
-  console.log(decimals);
+  const daiAddress = '0x6B175474E89094C44Da98b954EedeAC495271d0F';
+  const daiDecimals = await getDecimals(daiAddress, provider);
+  const DAI = new Token(targetChainId, daiAddress, Number(daiDecimals));
+
+  const WETH = WETH9[targetChainId];
+
+  const pair = await createPair(DAI, WETH, provider);
+  console.log(pair);
 };
 
 main();
--- src/utils/helpers.ts
+++ src/utils/helpers.ts
@@ -3,31 +3,43 @@ import {
   Network,
   Contract
 } from 'ethers';
-import { ChainId } from '@uniswap/sdk-core';
-import { abiERC20 } from '@metamask/metamask-eth-abis';
+import {
+  ChainId,
+  Token,
+  CurrencyAmount
+} from '@uniswap/sdk-core';
+import { Pair } from '@uniswap/v2-sdk';
+import IUniswapV2Pair from '@uniswap/v2-core/build/IUniswapV2Pair.json';
+import IUniswapV2ERC20 from '@uniswap/v2-core/build/IUniswapV2ERC20.json';
 
 import { INFURA_API_KEY } from '@/config/keys';
 
-const getProvider = () => {
+const getProvider = (chainId: ChainId) => {
   if (!INFURA_API_KEY) {
     throw new Error(`Infura API key is undefined: ${INFURA_API_KEY}`);
   }
 
-  try {
-    return  new JsonRpcProvider(
-      `https://mainnet.infura.io/v3/${INFURA_API_KEY}`,
-      // RE: https://github.com/ethers-io/ethers.js/issues/4377#issuecomment-1837559329
-      Network.from(ChainId.MAINNET),
-      { staticNetwork: true }
-    );
-  } catch (error) {
-    throw new Error(`Something went wrong: ${error}`);
+  let infuraEndpoint: string;
+
+  switch (chainId) {
+    case ChainId.MAINNET:
+      infuraEndpoint = `https://mainnet.infura.io/v3/${INFURA_API_KEY}`;
+      break;
+    default:
+      throw new Error('Invalid blockchain network!');
   }
+
+  return  new JsonRpcProvider(
+    infuraEndpoint,
+    // RE: https://github.com/ethers-io/ethers.js/issues/4377#issuecomment-1837559329
+    Network.from(chainId),
+    { staticNetwork: true }
+  );
 };
 
-const getDecimals = async (tokenAddress: string, provider: JsonRpcProvider): Promise<BigInt> => {
+const getDecimals = async (tokenAddress: string, provider: JsonRpcProvider): Promise<bigint> => {
   try {
-    const tokenContract = new Contract(tokenAddress, abiERC20, provider);
+    const tokenContract = new Contract(tokenAddress, IUniswapV2ERC20.abi, provider);
   
     return await tokenContract['decimals']();
   } catch (error) {
@@ -35,7 +47,27 @@ const getDecimals = async (tokenAddress: string, provider: JsonRpcProvider): Pro
   }
 };
 
+const createPair = async (tokenA: Token, tokenB: Token, provider: JsonRpcProvider): Promise<Pair> => {
+  try {
+    const pairAddress = Pair.getAddress(tokenA, tokenB);
+
+    const pairContract = new Contract(pairAddress, IUniswapV2Pair.abi, provider);
+    const reserves: bigint[] = await pairContract['getReserves']();
+    const [reserve0, reserve1] = reserves;
+    
+    const tokens = [tokenA, tokenB];
+    const [token0, token1] = tokens[0].sortsBefore(tokens[1]) ? tokens : [tokens[1], tokens[0]];
+    
+    const pair = new Pair(CurrencyAmount.fromRawAmount(token0, reserve0.toString()), CurrencyAmount.fromRawAmount(token1, reserve1.toString()));
+  
+    return pair;
+  } catch (error) {
+    throw new Error(`Something went wrong: ${error}`);
+  }
+};
+
 export {
   getProvider,
-  getDecimals
+  getDecimals,
+  createPair
 };
