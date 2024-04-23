63f45533819e43b467b0debd3a8ec3d20641d73f
Anton K. <antonkarlovskiy@outlook.com>
2024-04-24 00:46:38 +0200

    feat: integrate errorHandler

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,12 @@ import {
   ChainId,
   WETH9
 } from '@uniswap/sdk-core';
-import express, { Express, Request, Response } from 'express';
+import express, {
+  Express,
+  Request,
+  Response,
+  NextFunction
+} from 'express';
 
 import {
   buyTokensOnUniswapV2,
@@ -84,8 +89,17 @@ function processData(data: string): ProcessResponse {
 
 const app: Express = express();
 
-// Middleware to parse JSON request bodies
-app.use(express.json());
+type ErrorRequestHandler = (err: Error, req: Request, res: Response, next: NextFunction) => void;
+
+const errorHandler: ErrorRequestHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error('err.stack:', err.stack); // Log the error details for debugging
+
+  // Set a default status code (e.g., 500 Internal Server Error)
+  res.status(500).send('Something went wrong!');
+
+  // Pass the error to the next middleware (if not already handled)
+  next(err);
+};
 
 interface GetTradeInfoOnUniswapV2Query {
   chainId: string;
@@ -95,23 +109,32 @@ interface GetTradeInfoOnUniswapV2Query {
 }
 
 // GET request to /trade-info-on-uniswap-v2?chainId=1&inputTokenAddress=0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2&outputTokenAddress=0x6b175474e89094c44da98b954eedeac495271d0f&inputAmount=1000
-app.get('/trade-info-on-uniswap-v2', async (req: Request<any, any, any, GetTradeInfoOnUniswapV2Query>, res: Response<TradeInfoOnUniswapV2>) => {
-  const chainId = Number(req.query.chainId) as ChainId;
-  const inputTokenAddress = req.query.inputTokenAddress;
-  const outputTokenAddress = req.query.outputTokenAddress;
-  const inputAmount = Number(req.query.inputAmount);
-
-  // TODO: decimals
-  console.log('ray : ***** req.query => ', req.query);
-
-  const WETH = await createToken(inputTokenAddress, chainId);
-  const DAI = await createToken(outputTokenAddress, chainId);
-
-  const tradeInfoOnUniswapV2 = await getTradeInfoOnUniswapV2(WETH, DAI, inputAmount);
-  console.log('Trade info on Uniswap V2:', tradeInfoOnUniswapV2);
-
-  res.json(tradeInfoOnUniswapV2);
+app.get('/trade-info-on-uniswap-v2', async (req: Request<any, any, any, GetTradeInfoOnUniswapV2Query>, res: Response<TradeInfoOnUniswapV2>, next: NextFunction) => {
+  try {
+    const chainId = Number(req.query.chainId) as ChainId;
+    const inputTokenAddress = req.query.inputTokenAddress;
+    const outputTokenAddress = req.query.outputTokenAddress;
+    const inputAmount = Number(req.query.inputAmount);
+  
+    // TODO: decimals
+  
+    const WETH = await createToken(inputTokenAddress, chainId);
+    const DAI = await createToken(outputTokenAddress, chainId);
+  
+    const tradeInfoOnUniswapV2 = await getTradeInfoOnUniswapV2(WETH, DAI, inputAmount);
+    console.log('Trade info on Uniswap V2:', tradeInfoOnUniswapV2);
+  
+    res.json(tradeInfoOnUniswapV2);
+  } catch (error) {
+    next(error); // Pass the error to the next middleware
+  }
 });
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+
+// Register the error handling middleware in your Express app
+app.use(errorHandler);
 
 // POST request to /process endpoint
 app.post<ProcessRequest, ProcessResponse>('/process', (req, res) => {
